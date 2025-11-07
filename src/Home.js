f169796e0dc9f47fc39732477bf8e43856178892
kamuju rohini kumar <97517523+Kumarkrk@users.noreply.github.com>
2025-11-07 16:36:27 +0530

    Change blog fetch URL to production endpoint
    Updated the API endpoint for fetching blogs.

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Bloglist from "./blioglist";
 import useFetch from "./fetch";
 const Home = () => {
    
-const {blogs,ispending,erro}=useFetch("http://localhost:5000/blogs");
+const {blogs,ispending,erro}=useFetch("https://blog-tm0y.onrender.com");
     return ( <div className="home">
         {erro && <div> {erro}</div>}
      {ispending && <div>..loading kumar</div>}  
