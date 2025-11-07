f140d70657fad263d482eb2c395c24083a4db7c5
kamuju rohini kumar <97517523+Kumarkrk@users.noreply.github.com>
2025-11-07 16:29:59 +0530

    Change server port to use environment variable
    Updated server to listen on environment port or default to 5000.

--- data/server.js
+++ data/server.js
@@ -1,10 +1,13 @@
 import jsonServer from "json-server";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server running on port 3000");
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log("✅ JSON Server running");
 });
+
