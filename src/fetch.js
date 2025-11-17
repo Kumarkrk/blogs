e222e076352d0b49aec6e4513255d23e0063536e
kamuju rohini kumar <97517523+Kumarkrk@users.noreply.github.com>
2025-11-17 22:44:58 +0530

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,27 +1,24 @@
 import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-  const [blogs, setblogs] = useState([]);
+  const [blogs, setblogs] = useState(null);
   const [ispending, setIspending] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    setTimeout(() => {
-      fetch(url)
-        .then(res => {
-          if (!res.ok) throw Error("Data could not be fetched");
-          return res.json();
-        })
-        .then(data => {
-          setblogs(data.blogs);  // <-- make sure it's an array
-          setIspending(false);
-          setError(null);
-        })
-        .catch(err => {
-          setIspending(false);
-          setError(err.message);
-        });
-    }, 1000);
+    fetch(url)
+      .then(res => {
+        if (!res.ok) throw Error("Data could not be fetched");
+        return res.json();
+      })
+      .then(data => {
+        setblogs(data.blogs);
+        setIspending(false);
+      })
+      .catch(err => {
+        setError(err.message);
+        setIspending(false);
+      });
   }, [url]);
 
   return { blogs, ispending, error };
