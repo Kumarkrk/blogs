61d99ac913d97d3710fa35c8dfd8e739b396bcef
kamuju rohini kumar <97517523+Kumarkrk@users.noreply.github.com>
2025-11-17 22:44:03 +0530

    Update blogdetails.js

--- src/blogdetails.js
+++ src/blogdetails.js
@@ -1,41 +1,34 @@
-import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./fetch";
 
 const Blogdetails = () => {
-    const{id}=useParams();
-    const history=useHistory();
-    const handledelete=(id)=>
-    {
-        fetch("http://localhost:5000/blogs/"+blogs.id,{
-            method:'DELETE'
-            
-        }).then(()=>
-        {
-            history.push("/");
-        })}
-    const{blogs,ispending,error}=useFetch("http://localhost:5000/blogs/"+id);
-    return ( 
-        <div className="blogdetails">
-        
-        <div className="article">
-            {ispending && <div>Loading.. </div>}
-            {error && <div>{error}</div>}
-            {
-                blogs && <div className="article1">
-                    <article>
-                    <h1>{blogs.title}</h1>
-                    <p>{blogs.author}</p>
-                    <div className="a1">
-                        {blogs.body}
-                    </div>
-                    </article>
-                    <button onClick={()=>{handledelete(blogs.id)}}>delete</button>
-                
-                </div>
-            }
+  const { id } = useParams();
+  const history = useHistory();
+
+  const { blogs, ispending, error } = useFetch("/data/db.json");
+
+  const blog = blogs ? blogs.find(b => b.id === id) : null;
+
+  return (
+    <div className="blogdetails">
+      {ispending && <div>Loading..</div>}
+      {error && <div>{error}</div>}
+
+      {blog && (
+        <div className="article1">
+          <article>
+            <h1>{blog.title}</h1>
+            <p>{blog.author}</p>
+            <div className="a1">{blog.body}</div>
+          </article>
+
+          <button onClick={() => history.push("/")}>
+            Back
+          </button>
         </div>
-        </div>
-     );
-}
- 
+      )}
+    </div>
+  );
+};
+
 export default Blogdetails;
